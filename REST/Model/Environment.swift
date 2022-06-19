81869b8a78f694b6f6d23004273aa6cc2bfbe5af
Md Khaled Hasan Manna <manna@sharetrip.net>
2022-06-19 23:47:45 +0600

    create Environment class model

---
+++ REST/Model/Environment.swift
@@ -0,0 +1,18 @@
+//
+//  Environment.swift
+//  REST
+//
+//  Created by Md Khaled Hasan Manna on 19/6/22.
+//
+
+import Foundation
+
+class Environment {
+    static let shared = Environment()
+    var baseURL: URL
+    
+    private let devBaseURL = "https://fakerapi.it/api/v1"
+    private init() {
+        baseURL = URL(string: devBaseURL)!
+    }
+}
