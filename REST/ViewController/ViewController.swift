b1f8c2fca23e834d9631f5ad78d0992d44a07ee9
Md Khaled Hasan Manna <manna@sharetrip.net>
2022-06-20 23:06:33 +0600

    implemented tableview

--- REST/ViewController/ViewController.swift
+++ REST/ViewController/ViewController.swift
@@ -8,20 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.separatorStyle = .singleLine
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     let viewModel = AddressesViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.addSubview(tableView)
         viewModel.fetchAddresses()
-        
         viewModel.callBack.didSuccess = { address in
             print(address)
+            self.tableView.reloadData()
         }
         viewModel.callBack.didFailure = { error in
             print(error)
         }
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell.textLabel?.text = viewModel.cellItem(for: indexPath.row).city
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
